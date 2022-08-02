6a9f5c2a012c08c7a24c0430c5f9793c87506c97
Andy <andy380743909@gmail.com>
2022-08-02 16:04:26 +0800

    Initial Commit

---
+++ Tests/AppIconGenTests/AppIconGenTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import AppIconGen
+
+final class AppIconGenTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(AppIconGen().text, "Hello, World!")
+    }
+}
